e66875a501b3d53260919cfd9e1362beb3518898
icc50086-lab <icc50086@gmail.com>
2026-02-27 17:42:33 +0900

    宿泊予約アプリ 修正1

--- db/migrate/20260224082941_add_introduction_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddIntroductionToUsers < ActiveRecord::Migration[7.2]
-  def change
-    add_column :users, :introduction, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_24_090408) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_26_235726) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,27 +52,16 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_24_090408) do
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
-  create_table "rooms", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
-    t.text "detail"
-    t.integer "price"
-    t.string "address"
-    t.integer "user_id", null: false
+    t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_rooms_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -81,5 +70,4 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_24_090408) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "reservations", "rooms"
   add_foreign_key "reservations", "users"
-  add_foreign_key "rooms", "users"
 end
