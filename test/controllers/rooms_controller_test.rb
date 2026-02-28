7c06190ad5439dc75dda174eb20c2fc41100989b
icc50086-lab <icc50086@gmail.com>
2026-02-28 16:57:33 +0900

    宿泊予約アプリ 修正1

--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -5,15 +5,4 @@ class RoomsControllerTest < ActionDispatch::IntegrationTest
     get rooms_url
     assert_response :success
   end
-
-  test "should get show" do
-    room = Room.create!(name: "テスト", address: "東京", price: 1000)
-    get room_url(room)
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_room_url
-    assert_response :success
-  end
 end
