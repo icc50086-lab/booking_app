01953dc09e1245b53041dc121e3948cef2adfe1f
icc50086-lab <icc50086@gmail.com>
2026-03-01 09:46:11 +0900

    Fix tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_28_065256) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_28_093228) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_28_065256) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "detail"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class RoomsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:one)
+  end
+
   test "should get index" do
     get rooms_url
     assert_response :success
